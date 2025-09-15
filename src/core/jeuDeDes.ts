2a1574299a7906989bc34fdfef98eca85e69be0d
Andy Ly <splatnd@gmail.com>
2025-09-15 19:52:26 -0400

    Completion des étapes 3 et 4
    Il ne manque que un test dans jeuRouter-redemarrerJeu-lab0.test.ts, j'ai besoin de clarification.

--- src/core/jeuDeDes.ts
+++ src/core/jeuDeDes.ts
@@ -68,6 +68,16 @@ export class JeuDeDes {
         return JSON.stringify(resultat);
     }
 
+    public redemarrerJeu(): string {
+        this._joueurs.clear();
+        const resultat = {
+            message: "Le jeu a été redémarré."
+        };
+
+        // ne pas retourner l'objet de la couche domaine
+        return JSON.stringify(resultat);
+    }
+
     // d'autres méthodes (des RDCU)
     brasser() {
         this._d1.brasser();
